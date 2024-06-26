923e3bf3d24ee1b36fc3bf812096a4ff40576a61
euniceAtoboe99 <eunice.atoboe@ashesi.edu.gh>
2024-06-26 16:45:41 +0000

    My version

---
+++ apsel/lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:apsel/screens/signIn.dart';
+import 'package:apsel/screens/welcome.dart';
+import 'package:flutter/material.dart';
+
+
+void main() {
+  runApp( MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(brightness: Brightness.light),
+      darkTheme: ThemeData(brightness: Brightness.dark),
+      themeMode: ThemeMode.dark,
+      home: MyAppHome(),
+    );
+  }
+}
+
+class MyAppHome extends StatelessWidget {
+  const MyAppHome({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        title: Text('Welcome to Apsel'),
+      ),
+      body:  Center(
+        child:Column(
+          mainAxisAlignment:MainAxisAlignment.center,
+          children:[
+            Image.asset("assets/images/splash.png"),
+            Text("Drives sales and manage your store"),
+          ]
+        ) ,
+      ),
+    );
+  }
+}
---
+++ apsel/lib/screens/welcome.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Welcome extends StatelessWidget {
+  Welcome({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width:double.infinity,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/onboarding.jpg',),
+            fit:BoxFit.cover
+          ),
+        ),
+        child: Column(
+            mainAxisAlignment:MainAxisAlignment.center,
+            children: [
+              SizedBox(height:350,),
+            Image.asset('assets/images/splash.png'),
+            Text("Drive sales and manage your store"),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  foregroundColor: Colors.white, backgroundColor: Colors.blue, // Text color
+                  padding: EdgeInsets.symmetric(horizontal: 55, vertical: 20,),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(15)),
+                  ),
+                ),
+                onPressed: () {
+                  // Define the button's onPressed functionality here
+                },
+                child: Text('Get Started'),
+              )
+              ]
+        ),
+      ),
+    );
+  }
+}
